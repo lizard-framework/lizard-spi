bd300cd05ae553c7ee81ebd039b1864173fa198c
sagexueqi <job.xueqi@outlook.com>
2019-02-26 23:41:24 +0800

    add unit test 模拟并发场景下，线程安全的生成单例对象

--- src/test/java/io/lizardframework/spi/ExtensionTest.java
+++ src/test/java/io/lizardframework/spi/ExtensionTest.java
@@ -4,8 +4,11 @@ import io.lizardframework.spi.filter.Filter;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.*;
 
 /**
  * <p>测试用例</p>
@@ -40,4 +43,54 @@ public class ExtensionTest {
 		Assert.assertEquals(1, resultSet.size());
 	}
 
+	/**
+	 * 模拟并发场景下，线程安全的生成单例对象
+	 */
+	@Test
+	public void test3() throws InterruptedException {
+		// 模拟10线程并发
+		CyclicBarrier       cyclicBarrier = new CyclicBarrier(10);
+		CopyOnWriteArraySet resultSet     = new CopyOnWriteArraySet();
+
+		List<Callable<String>> callableList = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			SpiObjCyclicBarrier barrier = new SpiObjCyclicBarrier(cyclicBarrier, resultSet);
+			callableList.add(barrier);
+		}
+
+		// 主线程等待子线程全部执行完毕后再执行
+		ExecutorService pool = Executors.newFixedThreadPool(10);
+		pool.invokeAll(callableList);
+		pool.shutdown();
+
+		Assert.assertEquals(1, resultSet.size());
+	}
+
+	//.........................................//
+	static class SpiObjCyclicBarrier implements Callable<String> {
+
+		private CyclicBarrier               cyclicBarrier;
+		private CopyOnWriteArraySet<String> resultSet;
+
+		public SpiObjCyclicBarrier(CyclicBarrier cyclicBarrier) {
+			this.cyclicBarrier = cyclicBarrier;
+		}
+
+		public SpiObjCyclicBarrier(CyclicBarrier cyclicBarrier, CopyOnWriteArraySet<String> resultSet) {
+			this.cyclicBarrier = cyclicBarrier;
+			this.resultSet = resultSet;
+		}
+
+		@Override
+		public String call() throws Exception {
+			this.cyclicBarrier.await();
+
+			ExtensionLoader<Filter> loader   = ExtensionFactory.getExtensionLoader(Filter.class);
+			String                  hashcode = loader.getExtension().doFilter();
+			this.resultSet.add(hashcode);
+
+			System.out.println(hashcode);
+			return hashcode;
+		}
+	}
 }
